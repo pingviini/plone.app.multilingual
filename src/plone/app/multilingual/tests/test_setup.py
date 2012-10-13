76e3c1439f5f7cfab308e05ce7564608520aad4e
Jukka Ojaniemi <jukka.ojaniemi@gmail.com>
2012-10-13 20:06:52 +0200

    Passes pep8 now. Removed unused imports.

--- src/plone/app/multilingual/tests/test_setup.py
+++ src/plone/app/multilingual/tests/test_setup.py
@@ -1,19 +1,11 @@
 import unittest2 as unittest
 from Products.CMFCore.utils import getToolByName
 
-from plone.testing.z2 import Browser
-
-from plone.app.testing import TEST_USER_ID, TEST_USER_NAME, TEST_USER_PASSWORD
-from plone.app.testing import SITE_OWNER_NAME
-from plone.app.testing import login, logout
-from plone.app.testing import setRoles
-
-from plone.app.multilingual.testing import PLONEAPPMULTILINGUAL_INTEGRATION_TESTING
-from plone.app.multilingual.tests.utils import makeContent, makeTranslation
-from plone.app.multilingual.browser.vocabularies import AllContentLanguageVocabulary
+from plone.app.multilingual.testing import\
+        PLONEAPPMULTILINGUAL_INTEGRATION_TESTING
+from plone.app.multilingual.browser.vocabularies import\
+        AllContentLanguageVocabulary
 from plone.app.multilingual.browser.setup import SetupMultilingualSite
-
-import transaction
 
 
 class SetupViews(unittest.TestCase):
