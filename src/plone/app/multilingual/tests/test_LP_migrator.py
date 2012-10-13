29f1844168a54b93973d1c1e9e014fbbda1a92c7
Jukka Ojaniemi <jukka.ojaniemi@gmail.com>
2012-10-13 20:10:44 +0200

    Removed unused imports and made pep8 cleanup.

--- src/plone/app/multilingual/tests/test_LP_migrator.py
+++ src/plone/app/multilingual/tests/test_LP_migrator.py
@@ -1,18 +1,10 @@
 import unittest2 as unittest
-from plone.app.multilingual.testing import PLONEAPPMULTILINGUAL_INTEGRATION_TESTING
-from AccessControl import Unauthorized
+from plone.app.multilingual.testing import\
+        PLONEAPPMULTILINGUAL_INTEGRATION_TESTING
 from zope.component import getMultiAdapter
 from Products.CMFCore.utils import getToolByName
 
-from plone.app.testing import TEST_USER_ID, TEST_USER_NAME
-from plone.app.testing import SITE_OWNER_NAME
-from plone.app.testing import login, logout
-from plone.app.testing import setRoles
-
-from plone.app.testing import applyProfile
-
 from Products.LinguaPlone.tests.utils import makeContent, makeTranslation
-from Products.LinguaPlone.interfaces import ITranslatable
 from plone.multilingual.interfaces import ITranslationManager
 
 
@@ -36,9 +28,14 @@ class migrationLPToPAM(unittest.TestCase):
 
     def testSupportedLanguages(self):
         language_tool = getToolByName(self.portal, 'portal_languages')
-        self.failUnless(language_tool.getSupportedLanguages(), ['en', 'ca', 'es'])
+        self.failUnless(language_tool.getSupportedLanguages(),
+                        ['en', 'ca', 'es'])
 
     def testMigration(self):
-        migration_view = getMultiAdapter((self.portal, self.request), name='migration-view')
+        migration_view = getMultiAdapter((self.portal, self.request),
+                                         name='migration-view')
         migration_view()
-        self.assertEqual(ITranslationManager(self.doc1).get_translations(), {'ca': self.doc1_ca, 'en': self.doc1, 'es': self.doc1_es})
+        self.assertEqual(ITranslationManager(self.doc1).get_translations(),
+                         {'ca': self.doc1_ca,
+                          'en': self.doc1,
+                          'es': self.doc1_es})
