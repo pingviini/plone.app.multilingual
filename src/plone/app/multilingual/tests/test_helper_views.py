526a18fee76180aa5e40fe4b81e3ccae55d608e3
Jukka Ojaniemi <jukka.ojaniemi@gmail.com>
2012-10-13 21:44:27 +0200

    pep8.

--- src/plone/app/multilingual/tests/test_helper_views.py
+++ src/plone/app/multilingual/tests/test_helper_views.py
@@ -1,24 +1,23 @@
 import unittest2 as unittest
-from AccessControl import Unauthorized
-
-from zope.component import getMultiAdapter
 
 from Products.CMFCore.utils import getToolByName
 
 from plone.testing.z2 import Browser
 from plone.app.testing import TEST_USER_ID, TEST_USER_NAME, TEST_USER_PASSWORD
-from plone.app.testing import SITE_OWNER_NAME
-from plone.app.testing import login, logout
+from plone.app.testing import login
 from plone.app.testing import setRoles
 
-from plone.app.multilingual.testing import PLONEAPPMULTILINGUAL_FUNCTIONAL_TESTING
-from plone.app.multilingual.testing import PLONEAPPMULTILINGUAL_INTEGRATION_TESTING
+from plone.app.multilingual.testing import (
+        PLONEAPPMULTILINGUAL_FUNCTIONAL_TESTING,
+        PLONEAPPMULTILINGUAL_INTEGRATION_TESTING
+        )
 
 from plone.app.multilingual.tests.utils import makeContent, makeTranslation
-from plone.multilingual.interfaces import ITranslationManager
-from plone.multilingual.interfaces import ILanguage
 from plone.app.multilingual.browser.setup import SetupMultilingualSite
 from plone.app.multilingual.browser.utils import multilingualMoveObject
+
+from plone.multilingual.interfaces import ITranslationManager
+from plone.multilingual.interfaces import ILanguage
 
 
 import transaction
@@ -39,7 +38,9 @@ class PAMFuncTestHelperViews(unittest.TestCase):
         language_tool.addSupportedLanguage('es')
 
     def test_universal_link_view(self):
-        self.browser.addHeader('Authorization', 'Basic %s:%s' % (TEST_USER_NAME, TEST_USER_PASSWORD))
+        self.browser.addHeader('Authorization',
+                               'Basic %s:%s' % (TEST_USER_NAME,
+                                                TEST_USER_PASSWORD))
         self.browser.addHeader('Accept-Language', 'ca')
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         login(self.portal, TEST_USER_NAME)
@@ -78,9 +79,11 @@ class PAMIntTestHelperViews(unittest.TestCase):
 
         self.assertTrue(getattr(self.portal, 'en'))
 
-        self.portal.en.invokeFactory('Folder', 'new1', title=u"An archetypes based folder")
+        self.portal.en.invokeFactory('Folder', 'new1',
+                                     title=u"An archetypes based folder")
         new1 = self.portal.en['new1']
-        new1.invokeFactory('Document', 'doc1', title=u"An archetypes based doc")
+        new1.invokeFactory('Document', 'doc1',
+                           title=u"An archetypes based doc")
         transaction.commit()
 
         # Change the content language of the created folder to 'ca'
@@ -88,15 +91,18 @@ class PAMIntTestHelperViews(unittest.TestCase):
         self.assertTrue(self.portal.ca.new1)
         self.assertTrue(self.portal.ca.new1.doc1)
         self.assertEqual(ILanguage(self.portal.ca.new1).get_language(), 'ca')
-        self.assertEqual(ILanguage(self.portal.ca.new1.doc1).get_language(), 'ca')
+        self.assertEqual(ILanguage(self.portal.ca.new1.doc1).get_language(),
+                         'ca')
 
     def test_move_content_proper_language_folder_existing_translation_inside(self):
 
         self.assertTrue(getattr(self.portal, 'en'))
 
-        self.portal.en.invokeFactory('Folder', 'new11', title=u"An archetypes based folder")
+        self.portal.en.invokeFactory('Folder', 'new11',
+                                     title=u"An archetypes based folder")
         new11 = self.portal.en['new11']
-        new11.invokeFactory('Document', 'doc11', title=u"An archetypes based doc")
+        new11.invokeFactory('Document', 'doc11',
+                            title=u"An archetypes based doc")
         doc1_ca = makeTranslation(self.portal.en.new11.doc11, 'ca')
         doc1_ca.edit(title="Foo", language='ca')
         self.assertTrue(self.portal.ca.doc11)
@@ -107,9 +113,11 @@ class PAMIntTestHelperViews(unittest.TestCase):
         multilingualMoveObject(new11, 'ca')
         self.assertTrue(self.portal.ca.new11)
         self.assertTrue(self.portal.ca.new11.doc11)
-        self.assertEqual(ITranslationManager(self.portal.ca.new11.doc11).get_translations(),
-                         {'ca': self.portal.ca.new11.doc11})
+        self.assertEqual(ITranslationManager(
+                            self.portal.ca.new11.doc11).get_translations(),
+                            {'ca': self.portal.ca.new11.doc11})
         self.assertEqual(ITranslationManager(doc1_ca).get_translations(),
                          {'ca': doc1_ca})
         self.assertEqual(ILanguage(self.portal.ca.new11).get_language(), 'ca')
-        self.assertEqual(ILanguage(self.portal.ca.new11.doc11).get_language(), 'ca')
+        self.assertEqual(ILanguage(self.portal.ca.new11.doc11).get_language(),
+                         'ca')
